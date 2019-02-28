739591c0c95e350c99abdd49c7972f568f43f877
Yuhsuke Gozu <yukkenottuna@gmail.com>
2019-03-01 00:03:54 +0900

    firebaseの認証をテンプレに戻す

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyAE4vAw4rPOaBozCTjs8Q83uoM1QgnLw5s",
-    authDomain: "nuxt-firebase-8c0ca.firebaseapp.com",
-    databaseURL: "https://nuxt-firebase-8c0ca.firebaseio.com",
-    projectId: "nuxt-firebase-8c0ca",
-    storageBucket: "nuxt-firebase-8c0ca.appspot.com",
-    messagingSenderId: "713443344663"
+    apiKey: "API_KEY",
+    authDomain: "APP_ID.firebaseapp.com",
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "1234567890"
   })
 }
 
